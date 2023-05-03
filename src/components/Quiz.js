8004b6362345072e7a94427d29a68b30e3ee2e9c
amankr1635 <amankr.1635@gmail.com>
2023-05-03 18:28:43 +0530

    first Commit

---
+++ src/components/Quiz.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { questions } from "./Question.js";
+import "./Quiz.css";
+
+export default function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const [score, setScore] = React.useState(0);
+  const [showScore, setShowScore] = React.useState(false);
+
+  const handleClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <div className="app">
+      {showScore ? (
+        <section className="showScore-section">
+          Your score is {score} out of {questions.length}
+        </section>
+      ) : (
+        <>
+          <section className="question-section">
+            <h1>
+              Question {currentQuestion + 1}/{questions.length}
+            </h1>
+            <p>{questions[currentQuestion].questionText}</p>
+          </section>
+
+          <section className="answer-section">
+            {questions[currentQuestion].answerOptions.map((item) => (
+              <button onClick={() => handleClick(item.isCorrect)}>
+                {item.answerText}
+              </button>
+            ))}
+          </section>
+        </>
+      )}
+    </div>
+  );
+}
